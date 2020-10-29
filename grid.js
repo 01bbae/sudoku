1d26f6bd11bb63a491dff4f56bde5477d9dc506b
01bbae <41602923+01bbae@users.noreply.github.com>
2020-10-29 02:23:21 -0700

    First commit

---
+++ grid.js
@@ -0,0 +1,28 @@
+var canvas = document.getElementById("gamescreen");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle ="#FF0000";
+ctx.fillRect(0,0,500,500);
+
+
+
+
+
+
+
+
+
+// var grid = new Array(3)
+// for(let i=0;i<3;++i){
+//     grid[i] = new Array(3);
+//     for(let j=0;j<3;++j){
+//         grid[i][j] = new Array(3);
+//         for(let k=0;k<3;++k){
+//             grid[i][j][k] = new Array(3);
+//             for(let l=0;l<3;++l){
+//                 grid[i][j][k][l] = new Array(3);
+//             }
+//         }
+//     }
+// }
+
+// console.log(grid)
